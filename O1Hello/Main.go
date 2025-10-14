d7deeb6551825a11a30e83441453de81df31a242
SuvraneelSaha <suvroneel2001@gmail.com>
2025-10-14 15:00:13 +0530

    Lexer in go

--- O1Hello/Main.go
+++ O1Hello/Main.go
@@ -9,3 +9,10 @@ func main()  {
 // go env GOPATH --  for checking the path of go lang 
 // go help -- lists all commands for help 
 // go help run -- for help regarding run command as an Example 
+
+/*
+A lexer basically does checking of the code according to the syntax of the language 
+and specificiatons during precompilation time . 
+It does not corrects any logical and runtime errors . 
+
+*/
