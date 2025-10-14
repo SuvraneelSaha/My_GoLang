a64e0d23e93a5fba98d8f2a1988c17b8c69a692d
SuvraneelSaha <suvroneel2001@gmail.com>
2025-10-14 13:49:21 +0530

    GOPATH AND help command in go

--- O1Hello/Main.go
+++ O1Hello/Main.go
@@ -6,3 +6,6 @@ func main()  {
 	fmt.Println("Hello, World from AgginDev");
 }
 
+// go env GOPATH --  for checking the path of go lang 
+// go help -- lists all commands for help 
+// go help run -- for help regarding run command as an Example 
